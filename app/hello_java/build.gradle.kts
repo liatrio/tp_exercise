443ec7ee5c7e0d7e80401061fe32774a1f5fa86d
wombatpiobair <bob.combs@liatrio.com>
2023-02-14 11:16:12 -0800

    add docker image generation

--- app/hello_java/build.gradle.kts
+++ app/hello_java/build.gradle.kts
@@ -1,12 +1,19 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.0.2"
+	id("org.springframework.boot") version "2.0.3.RELEASE"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("com.bmuschko.docker-spring-boot-application") version "9.1.0"
+
 }
+
 
 group = "com.liatrio"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
 	mavenCentral()
@@ -19,4 +26,14 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+springBoot {
+	buildInfo()
+}
+
+docker {
+	springBootApplication {
+		baseImage.set("openjdk:8-alpine")
+	}
 }
